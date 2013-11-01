c7360e279c58058c1ee9e79bd1fc7597be0a3c97
matt <matt@matts-air.home>
2013-10-31 21:41:53 -0400

    Including Foundation framework, and some boilerplate code

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-console.log('ready');
+$.noConflict();
+(function($){
+    window.appJS = window.appJS || {};
+
+    appJS.main = function() {
+        var init = function() {
+            console.log('app js init');
+        };
+
+        return {
+            init: init
+        }
+    }();
+
+    $(document).ready(function() {
+        appJS.main.init();
+    });
+})(jQuery);
--- resources/js/scripts.combined.js
+++ resources/js/scripts.combined.js
@@ -1,1 +1,3 @@
+<!--#include file="jquery-1.10.2.min.js" -->
+<!--#include file="foundation.min.js" -->
 <!--#include file="app.js" -->
